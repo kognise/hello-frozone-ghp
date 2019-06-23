5c5f0859aef35296c9550f19971014b5c4c8903a
Felix Mattick <felix.mattick@gmail.com>
2019-06-23 11:19:10 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default () => (
+  <div>
+    <h1>Hello, Frozone!</h1>
+
+    <style jsx>{`
+      h1 {
+        font-family: sans-serif;
+        font-size: 3em;
+        text-align: center;
+        color: #23ffd3;
+      }
+    `}</style>
+    <style jsx global>{`
+      body {
+        background: #000000;
+      }
+    `}</style>
+  </div>
+)
